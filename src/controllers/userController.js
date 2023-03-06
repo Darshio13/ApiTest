9595ef9a3e8f19470c0af4cb158c72c1e828163c
Darsh <santos.m.diego.a@gmail.com>
2023-03-05 23:54:46 -0600

    send mail lol i forgot

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,65 +2,72 @@ const nodemailer = require('nodemailer');
 
 const Usuario = require("../models/usuario");
 
-exports.userGet= (req, res)=>{
+exports.userGet = (req, res) => {
     Usuario.query()
-    .where('correo_electronico', '=', req.params.correo)
-    .where('password', '=', req.params.password)
-    .then((results)=>{
-        if(results.length>0)
-        {
-            res.json(results);
-        }
-        else
-        {
-            res.json("No se encontro el usuario")
-        }
-    })
-}
-
-exports.userPost= (req, res)=>{
-    Usuario.query()
-    .insertAndFetch({
-    nombre:req.params.nombre,
-    apellidos:req.params.apellidos,
-    nombre_usuario:req.params.nombre_usuario,
-    correo_electronico:req.params.correo_electronico,
-    password:req.params.password,
-    tipo_usuario:1
-        
-})
-    .then((results)=>{
-        console.log(results);
-
-        //Enviar correo
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-              user: 'santos.m.diego.a@gmail.com',
-              pass: 'enbatovbhogpsdyj'
+        .where('correo_electronico', '=', req.params.correo)
+        .where('password', '=', req.params.password)
+        .then((results) => {
+            if (results.length > 0) {
+                res.json(results);
             }
-          });
-          
-          const mailOptions = {
-            from: 'santos.m.diego.a@gmail.com',
-            to: results[0].correo_electronico,
-            subject: 'Prueba',
-            text: 'Correo Exito'
-          };
-
-        res.json("Se registro el usuario")
-    })
+            else {
+                res.json("No se encontro el usuario")
+            }
+        })
 }
 
-exports.userPutEstatus= (req, res)=>{    
-    
+exports.userPost = (req, res) => {
     Usuario.query()
-    .where('id_usuario', '=', req.params.id)
-    .patch({
-        estatus_confirmacion:1
-    })
-    .then((results)=>{
-        res.json("Se ha verificado al usuario")
-    })
+        .insertAndFetch({
+            nombre: req.params.nombre,
+            apellidos: req.params.apellidos,
+            nombre_usuario: req.params.nombre_usuario,
+            correo_electronico: req.params.correo_electronico,
+            password: req.params.password,
+            tipo_usuario: 1
+
+        })
+        .then((results) => {
+            console.log(results);
+
+            //Enviar correo
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: 'santos.m.diego.a@gmail.com',
+                    pass: 'enbatovbhogpsdyj'
+                }
+            });
+
+            const mailOptions = {
+                from: 'santos.m.diego.a@gmail.com',
+                to: results[0].correo_electronico,
+                subject: 'Prueba',
+                text: 'Correo Exito'
+            };
+
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                    // do something useful
+                }
+            });
+
+            res.json("Se registro el usuario")
+        })
+}
+
+exports.userPutEstatus = (req, res) => {
+
+    Usuario.query()
+        .where('id_usuario', '=', req.params.id)
+        .patch({
+            estatus_confirmacion: 1
+        })
+        .then((results) => {
+            res.json("Se ha verificado al usuario")
+        })
 
 }
